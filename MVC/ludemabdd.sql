2c38213b48e1fe0be40f838e7c5059bbec37d181
AliJomaa99 <ali.pro.jom@orange.fr>
2019-01-02 14:11:53 +0100

    bdd

--- MVC/ludemabdd.sql
+++ MVC/ludemabdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  Dim 30 déc. 2018 à 19:24
+-- Généré le :  mer. 02 jan. 2019 à 13:08
 -- Version du serveur :  5.7.23
 -- Version de PHP :  7.2.10
 
@@ -21,6 +21,52 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `ludemabdd`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `idClient` int(11) NOT NULL AUTO_INCREMENT,
+  `loginUtilisateur` varchar(25) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `age` int(11) NOT NULL,
+  `sexe` varchar(10) NOT NULL,
+  `note` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`idClient`)
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`idClient`, `loginUtilisateur`, `prenom`, `nom`, `age`, `sexe`, `note`) VALUES
+(1, 'Ali', 'walid', 'messaoudi', 12, 'homme', 'fefe'),
+(2, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(3, 'a', 'dfbdf', 'f bdf', 115, 'Homme', 'yl;yi;fuiiiiiiiiu:'),
+(4, 'a', 'dfbdf', 'f bdf', 115, 'Homme', 'yl;yi;fuiiiiiiiiu:'),
+(5, 'a', 'dfbdf', 'f bdf', 115, 'Homme', 'yl;yi;fuiiiiiiiiu:'),
+(6, 'a', 'grg', 'fef', 7, 'Homme', 'iklui'),
+(7, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(8, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(9, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(10, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(11, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(12, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(13, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(14, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(15, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(16, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(17, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(18, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(19, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(20, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(21, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb'),
+(22, 'a', 'ali', 'Wali d', 15, 'Homme', 'fbfb');
 
 -- --------------------------------------------------------
 
@@ -114,25 +160,14 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   PRIMARY KEY (`login`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-
--- --------------------------------------------------------
-
 --
--- Structure de la table `client`
+-- Déchargement des données de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
-  `idClient` int NOT NULL AUTO_INCREMENT,
-  `loginUtilisateur` varchar(25)  NOT NULL,
-  `prenom` varchar(30) NOT NULL,
-  `nom` varchar(30) NOT NULL,
-  `age` int  NOT NULL,
-  `sexe` varchar(10) NOT NULL,
-  `note` varchar(300) DEFAULT NULL,
-  PRIMARY KEY (`idClient`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+INSERT INTO `utilisateur` (`login`, `email`, `mdp`, `prenom`, `nom`, `historique_compo`) VALUES
+('a', 'a', 'a', 'a', 'a', 'a');
 COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
