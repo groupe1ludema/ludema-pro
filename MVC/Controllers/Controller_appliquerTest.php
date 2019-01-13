d9de3d7ca6cf78b18fc3a0e480624563144b433e
Emeric Dupil <emeric.dupil@gmail.com>
2019-01-13 14:01:47 +0100

    Axe réparé + Description test retirée

--- MVC/Controllers/Controller_appliquerTest.php
+++ MVC/Controllers/Controller_appliquerTest.php
@@ -10,12 +10,12 @@ class Controller_appliquerTest extends Controller {
     $n=sizeof($_SESSION['listTestSelectionnes']);
     for($i=0;$i<$n;$i++){
       $rep[$i]=$m->get_questions($_SESSION['listTestSelectionnes'][$i]['catgeorie']);
-      $description[$i]=$m->get_descriptionTest($_SESSION['listTestSelectionnes'][$i]['catgeorie']);
+    //  $description[$i]=$m->get_descriptionTest($_SESSION['listTestSelectionnes'][$i]['catgeorie']); // On ne met pas de description
     }
     $data = [
             /*"listeTest" => $m->get_question($_SESSION['listTestSelectionnes']),*/
             "questions" => $rep,
-            "description"=>$description,
+          //  "description"=>$description,
             "nbtest"=>$n
         ];
     $this->render("appliquerTest",$data);
